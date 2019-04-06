4958afe211e8e4ab1462fd68dc625d47780da2fc
susomi_notebook <whymeiee789@gmail.com>
2019-04-05 20:19:02 -0400

    gathered pres info from crawl links--with minor bugs

--- SI507project_tools.py
+++ SI507project_tools.py
@@ -13,6 +13,7 @@ START_URL = "https://millercenter.org/president"
 PROGRAM_CACHE = Cache(FNAME)
 # Function which either gets data from cache or creates new get request, then returns data
 def scrape_function(some_url):
+    print ("getting data")
     data = PROGRAM_CACHE.get_data(some_url)
     if not data:
         print("MAKING NEW REQUEST")
@@ -22,11 +23,11 @@ def scrape_function(some_url):
 
 # scrape main page and turn data into BeautifulSoup object
 main_page = scrape_function(START_URL)
-soup = BeautifulSoup(main_page, features="html.parser")
+main_soup = BeautifulSoup(main_page, features="html.parser")
 
 # find_all a tag data from table on main page
-pres_table = soup.find(id="block-mainpagecontent")
-a_tags_list = soup.find_all('a', href=True)[3:47]
+pres_table = main_soup.find(id="block-mainpagecontent")
+a_tags_list = main_soup.find_all('a', href=True)[3:47]
 
 # create crawl links using main mage URL and a tag href_values
 crawl_links = []
@@ -35,8 +36,41 @@ for a_tag in a_tags_list:
     link = f'https://millercenter.org{href_value}'
     crawl_links.append(link)
 
-print (crawl_links)
+# print (crawl_links)
 
+count = 0
+# iterate through list of crawl_links
+for link in crawl_links:
+    if count >= 1:
+        break
+    else:
+        # get scraped data from cache or get request to site
+        crawl_page = scrape_function(link)
+        # make BeautifulSoup object from page
+        crawl_soup = BeautifulSoup(crawl_page, features="lxml")
+        # target pres. facts wrapper element
+        facts_wrapper = crawl_soup.find("div", {"class": "fast-facts-wrapper"})
+        # create list of the all the div_text in facts_wrapper
+        div_text_list = facts_wrapper.find_all('div', text=True)
+        # iterate through list and grab pres info
+        for div_text in div_text_list:
+            full_name = div_text_list[0].text.split()
+            l_name = full_name[1]
+            f_name = full_name[0]
+            birth_place = div_text_list[1].text
+            education = div_text_list[2].text
+            religion = div_text_list[3].text
+            career = div_text_list[4].text
+            pres_number =  div_text_list[-2].text
+
+        time_text_list = facts_wrapper.find_all("time")
+        for time_text in time_text_list:
+            birth_date = time_text_list[0].text
+            inauguration_date = time_text_list[2].text
+        # create row from pres info
+        row = [pres_number, l_name, f_name, birth_date, birth_place, education, career, religion]
+        # print (row)
+    count +=1
 #
 # with open("example_csv_table.csv", "w", newline="") as example_fh:
 #     writer = csv.writer(example_fh)
