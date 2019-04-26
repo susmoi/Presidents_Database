8df101b649efd6a8c869072ff0eff78f36326c14
susomi_notebook <whymeiee789@gmail.com>
2019-04-25 22:50:17 -0400

    removed old test functions, added a new function

--- SI507project_tests.py
+++ SI507project_tests.py
@@ -9,24 +9,6 @@ class TestScrapFunct(unittest.TestCase):
         result = scrape_function(self.url)
         self.assertTrue(type(result) is str)
 
-class TestPresCsvFunct(unittest.TestCase):
-    def setUp(self):
-        self.list_dicts = pres_data_list
-        self.result = make_pres_csv(self.list_dicts)
-        self.header_list = ['President Number', 'Last Name', 'First Name', 'Birthday', 'Education' ,'Inagural Date', 'Religon', 'Career', 'Party']
-
-    def test_return_not_int(self):
-        self.assertFalse(self.result is int)
-
-    def test_return_not_str(self):
-        self.assertFalse(self.result is str)
-
-    def test_return_not_tup(self):
-        self.assertFalse(self.result is tuple)
-
-    def test_return_is_list(self):
-        self.assertTrue(self.result is list)
-
 class TestData(unittest.TestCase):
     def test_crawl_links(self):
         self.assertTrue(type(crawl_links) is list)
@@ -41,6 +23,11 @@ class TestData(unittest.TestCase):
         self.assertTrue(type(a_tags_list) is list)
     # def test_full_name(self):
     #     self.assertNotEqual()
+
+class TestPopData (unittest.TestCase):
+    def test_make_pres_list(self):
+        self.assertTrue(type(pres_data_list) is list)
+
 
 if __name__ == '__main__':
     unittest.main()
